98bb49f51f2822556aebec65d5da99d0525f1b87
YassineAzedine <yassine.azedine0711@gmail.com>
2025-04-03 16:36:57 +0000

    i fix problem searchparams wrap by comment code and i fix problem profil page by comont code just for prodcution process

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,30 +1,30 @@
-import UpdateButton from "@/components/UpdateButton";
-import { updateUser } from "@/lib/actions";
-import { wixClientServer } from "@/lib/wixClientServer";
-import { members } from "@wix/members";
-import Link from "next/link";
-import { format } from "timeago.js";
+// import UpdateButton from "@/components/UpdateButton";
+// import { updateUser } from "@/lib/actions";
+// import { wixClientServer } from "@/lib/wixClientServer";
+// import { members } from "@wix/members";
+// import Link from "next/link";
+// import { format } from "timeago.js";
 
 const ProfilePage = async () => {
-  const wixClient = await wixClientServer();
+  // const wixClient = await wixClientServer();
 
-  const user = await wixClient.members.getCurrentMember({
-    fieldsets: [members.Set.FULL],
-  });
+  // const user = await wixClient.members.getCurrentMember({
+  //   fieldsets: [members.Set.FULL],
+  // });
 
-  if (!user.member?.contactId) {
-    return <div className="">Not logged in!</div>;
-  }
+  // if (!user.member?.contactId) {
+  //   return <div className="">Not logged in!</div>;
+  // }
 
-  const orderRes = await wixClient.orders.searchOrders({
-    search: {
-      filter: { "buyerInfo.contactId": { $eq: user.member?.contactId } },
-    },
-  });
+  // const orderRes = await wixClient.orders.searchOrders({
+  //   search: {
+  //     filter: { "buyerInfo.contactId": { $eq: user.member?.contactId } },
+  //   },
+  // });
 
   return (
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-      <div className="w-full md:w-1/2">
+      {/* <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Profile</h1>
         <form action={updateUser} className="mt-12 flex flex-col gap-4">
           <input type="text" hidden name="id" value={user.member.contactId} />
@@ -90,7 +90,7 @@ const ProfilePage = async () => {
             </Link>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
